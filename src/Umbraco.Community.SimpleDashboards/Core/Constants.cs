83e68cf2139e4020ab8381f989bac1aedc03097d
jcdcdev <james@jcdc.dev>
2023-08-22 21:19:14 +0100

    fix lang version and constants

--- src/Umbraco.Community.SimpleDashboards/Core/Constants.cs
+++ src/Umbraco.Community.SimpleDashboards/Core/Constants.cs
@@ -7,28 +7,24 @@ public static class Constants
     public const string Area = "simpledashboards";
 
     public static string ExampleViewComponent(string name) =>
-        $$"""
-          using Microsoft.AspNetCore.Mvc;
-          using Umbraco.Community.SimpleDashboards.Web;
+        $@"using Microsoft.AspNetCore.Mvc;
+using Umbraco.Community.SimpleDashboards.Web;
 
-          namespace {{_nameSpace}}.Views.Components;
+namespace {_nameSpace}.Views.Components;
 
-          public class {{name}}ViewComponent : DashboardViewComponent
-          {
-              public override IViewComponentResult Invoke(DashboardModel model)
-              {
-                    return Content($"Hello {model.Dashboard.Alias}");
-              }
-          }
-          """;
+public class {name}ViewComponent : DashboardViewComponent
+{{
+    public override IViewComponentResult Invoke(DashboardModel model)
+    {{
+        return Content($""Hello {{model.Dashboard.Alias}}"");
+    }}
+}}";
 
     public static string ExampleView =
-        """
-        @inherits Umbraco.Community.SimpleDashboards.Web.DashboardViewPage
+        @"@inherits Umbraco.Community.SimpleDashboards.Web.DashboardViewPage
 
-        <h1>Hello Umbraco</h1>
-        <p>My Dashboard is: @Model.Dashboard.Alias</p>
-        """;
+<h1>Hello Umbraco</h1>
+<p>My Dashboard is: @Model.Dashboard.Alias</p>";
 
     private static readonly string _nameSpace = Assembly.GetEntryAssembly()?.GetName()?.Name ?? "YourNamespace";
 }
