4e999d264883a3e041829d5016ef11374bbe11d2
jcdcdev <james@jcdc.dev>
2024-11-12 23:51:47 +0000

    fix: build issue

--- src/Umbraco.Community.SimpleDashboards/Web/SimpleDashboard.cs
+++ src/Umbraco.Community.SimpleDashboards/Web/SimpleDashboard.cs
@@ -1,5 +1,6 @@
 ﻿using Humanizer;
 using Umbraco.Community.SimpleDashboards.Core.Models;
+using Umbraco.Extensions;
 
 namespace Umbraco.Community.SimpleDashboards.Web;
 
@@ -8,7 +9,7 @@ public abstract class SimpleDashboard : ISimpleDashboard
     public virtual string ViewPath => $"~/Views/Dashboards/{Alias}.cshtml";
     public virtual string ViewComponent => $"{Alias}Dashboard";
     public virtual string Label => Name;
-    public string Alias => GetType().Name.TrimEnd("Dashboard");
+    public string Alias => GetType().Name.TrimEndExact("Dashboard");
     public string PathName => Alias.Kebaberize();
     public virtual int Weight => 100;
     public virtual string Name => Alias;
