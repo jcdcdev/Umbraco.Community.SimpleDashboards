61e04cadfa17abee1ad973137f2a01cddbd156b3
jcdcdev <james@jcdc.dev>
2025-04-21 20:16:52 +0100

    chore: update FE dependencies

--- src/Umbraco.Community.SimpleDashboards.Client/openapi-ts.config.ts
+++ src/Umbraco.Community.SimpleDashboards.Client/openapi-ts.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from '@hey-api/openapi-ts';
+import { defineConfig, defaultPlugins } from '@hey-api/openapi-ts';
 
 export default defineConfig({
     input: 'http://localhost:54813/umbraco/swagger/SimpleDashboards/swagger.json',
+    plugins: [
+        ...defaultPlugins,
+        'legacy/fetch',
+        '@hey-api/schemas',
+        {
+            dates: true,
+            name: '@hey-api/transformers',
+        },
+        {
+            enums: 'javascript',
+            name: '@hey-api/typescript',
+        },
+        {
+            name: '@hey-api/sdk',
+            transformer: true,
+        },
+    ],
     output: {
         format: 'prettier',
         path: './src/api',
-    },
-    client: "legacy/fetch",
-    types: {
-        enums: 'typescript',
-    },
+    }
 });
